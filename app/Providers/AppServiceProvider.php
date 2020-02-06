33765217cf49332afafc91656249a5d82b926ecf
Indra Gunanda <indra.gunanda@gmail.com>
2020-02-06 13:22:46 +0700

    Migrationg Generator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
     {
       if ($this->app->environment() !== 'production') {
            $this->app->register(\Sven\ArtisanView\ServiceProvider::class);
+           $this->app->register(\Way\Generators\GeneratorsServiceProvider::class);
+           $this->app->register(\Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
        }
     }
 }
